1283ccd3e8f7ac84a9619a8d42f2e1501f5b1444
holinder4s <holinder4s@gmail.com>
2021-01-07 17:33:04 +0900

    delete build command in run.sh & debug.sh

--- debug.sh
+++ debug.sh
@@ -1,4 +1,3 @@
-make
 qemu-system-x86_64 -s -S -no-kvm -L . -m 64 -fda ./Disk.img -rtc base=localtime -M pc &
 gdb -ix ./Disk.img\
     -ex 'target remote localhost:1234'\
--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-make
 qemu-system-x86_64 -L . -m 64 -fda ./Disk.img -rtc base=localtime -M pc
