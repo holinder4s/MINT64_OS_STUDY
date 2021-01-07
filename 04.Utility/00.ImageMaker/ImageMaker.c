1b4ec31e99a434ee34fdec38a37c8f7f8acff3b6
holinder4s <holinder4s@gmail.com>
2021-01-07 17:46:38 +0900

    Create ImageMaker.c

---
+++ 04.Utility/00.ImageMaker/ImageMaker.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <io.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define BYTESOFSECTOR   512
+
+// main 함수
+int main(void) {
+    
+    return 0;
+}
