10d54fb8ae71297e478935391cdecb23bba727db
holinder4s <holinder4s@gmail.com>
2021-01-08 18:10:01 +0900

    Create Page Directory Table

--- 01.Kernel32/Source/Page.c
+++ 01.Kernel32/Source/Page.c
@@ -23,4 +23,16 @@ void kInitializePageTables(void) {
     for(i=0; i<64; i++) {
         kSetPageEntryData(&(pstPDPTEntry[i]), 0, 0x102000 + (i * PAGE_TABLESIZE), PAGE_FLAGS_DEFAULT, 0);
     }
+
+    // 페이지 디렉터리 테이블 생성
+    // 하나의 페이지 디렉터리가 1GB까지 매핑 가능
+    // 여유있게 64개의 페이지 디렉터리를 생성하여 총 64GB까지 지원
+    pstPDEntry = (PDENTRY *)0x102000;
+    dwMappingAddress = 0;
+    for(i=0; i<PAGE_MAXENTRYCOUNT*64; i++) {
+        // 32비트로는 상위 어드레스를 표현할 수 없으므로, MB 단위로 계산한 다음
+        // 최종 결과를 다시 4KB로 나누어 32비트 이상의 어드레스를 계산함
+        kSetPageEntryData(&(pstPDEntry[i]), (i * (PAGE_DEFAULTSIZE >> 20)) >> 12, dwMappingAddress, PAGE_FLAGS_DEFAULT | PAGE_FLAGS_PS, 0);
+        dwMappingAddress += PAGE_DEFAULTSIZE;
+    }
 }
