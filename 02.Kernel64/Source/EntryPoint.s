a4fd058eb21c59953a15d9a54ebf2dbdb9f2bdc2
holinder4s <holinder4s@gmail.com>
2021-01-11 17:25:20 +0900

    Create & Initialize IA-32e's Stack

--- 02.Kernel64/Source/EntryPoint.s
+++ 02.Kernel64/Source/EntryPoint.s
@@ -14,3 +14,8 @@ START:
     mov es, ax          ; ES 세그먼트 셀렉터에 설정
     mov fs, ax          ; FS 세그먼트 셀렉터에 설정
     mov gs, ax          ; GS 세그먼트 셀렉터에 설정
+    
+    ;   스택을 0x600000~0x6FFFFF 영역에 1MB 크기로 생성
+    mov ss, ax          ; SS 세그먼트 셀렉터에 설정
+    mov rsp, 0x6FFFF8   ; RSP 레지스터의 어드레스를 0x6FFFF8로 설정
+    mov rbp, 0x6FFFF8   ; RBP 레지스터의 어드레스를 0x6FFFF8로 설정
