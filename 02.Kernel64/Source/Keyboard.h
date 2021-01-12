7f7cc67f3b499dc2b89d2363fd26ccc8cc3ce393
holinder4s <holinder4s@gmail.com>
2021-01-12 16:41:14 +0900

    define scan code mapping table entry struct

---
+++ 02.Kernel64/Source/Keyboard.h
@@ -0,0 +1,20 @@
+#ifndef __KEYBOARD_H__
+#define __KEYBOARD_H__
+
+#include "Types.h"
+
+// 구조체
+#pragma pack(push, 1)
+
+// 스캔 코드 테이블을 구성하는 항목
+typedef struct kKeyMappingEntryStruct {
+    // Shift 키나 Caps Lock 키와 조합되지 않는 ASCII 코드
+    BYTE bNormalCode;
+
+    // Shift 키나 Caps Lock 키와 조합된 ASCII 코드
+    BYTE bCombinedCode;
+} KEYMAPPINGENTRY;
+
+#pragma pack(pop)
+
+#endif /*__KEYBOARD_H__*/
