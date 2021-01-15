f7f0eee40818bc16cfd198dc796a37e6cbe6c08a
holinder4s <holinder4s@gmail.com>
2021-01-15 18:23:09 +0900

    implement kMemCpy(...)

--- 02.Kernel64/Source/Utility.c
+++ 02.Kernel64/Source/Utility.c
@@ -8,3 +8,14 @@ void kMemSet(void *pvDestination, BYTE bData, int iSize) {
         ((char *)pvDestination)[i] = bData;
     }
 }
+
+// 메모리 복사
+int kMemCpy(void *pvDestination, const void *pvSource, int iSize) {
+    int i;
+
+    for(i=0; i<iSize; i++) {
+        ((char *)pvDestination)[i] = ((char *)pvSource)[i];
+    }
+
+    return iSize;
+}
