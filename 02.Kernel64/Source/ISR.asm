fc924211a2f0b88e47ac393b56b23b04e504b93c
holinder4s <holinder4s@gmail.com>
2021-01-18 20:00:17 +0900

    add KSAVECONTEXT macro in ISR.asm

---
+++ 02.Kernel64/Source/ISR.asm
@@ -0,0 +1,38 @@
+[BITS 64]           ; 이하의 코드는 64비트 코드로 설정
+
+SECTION .text       ; text 섹션(세그먼트)을 정의
+
+; 콘텍스트를 저장하고 셀렉터를 교체하는 매크로
+%macro KSAVECONTEXT 0       ; 파라미터를 전달받지 않는 KSAVECONTEXT 매크로 정의
+    ; RBP 레지스터부터 GS 세그먼트 셀렉터까지 모두 스택에 삽입
+    push rbp
+    mov rbp, rsp
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rdi
+    push rsi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov ax, ds      ; DS 세그먼트 셀렉터와 ES 세그먼트 셀렉터는 스택에 직접 삽입할 수 없으므로, RAX 레지스터에 저장한 후 스택에 삽입
+    push rax
+    mov ax, es
+    push rax
+    push fs
+    push gs
+
+    ; 세그먼트 셀렉터 교체
+    mov ax, 0x10    ; AX 레지스터에 커널 데이터 세그먼트 디스크립터 저장
+    mov ds, ax      ; DS 세그먼트 셀렉터부터 FS 세그먼트 셀렉터까지 모두 커널 데이터 세그먼트로 교체
+    mov es, ax
+    mov gs, ax
+    mov fs, ax
+%endmacro           ; 매크로 끝
