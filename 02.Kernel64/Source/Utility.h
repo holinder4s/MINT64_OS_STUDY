5041a18980fb52465324d4da0461c8fa19c5f811
holinder4s <holinder4s@gmail.com>
2021-01-19 14:26:55 +0900

    implement kSetInterruptFlag(...)

--- 02.Kernel64/Source/Utility.h
+++ 02.Kernel64/Source/Utility.h
@@ -7,5 +7,6 @@
 void kMemSet(void *pvDestination, BYTE bData, int iSize);
 int kMemCpy(void *pvDestination, const void *pvSource, int iSize);
 int kMemCmp(void *pvDestination, const void *pvSource, int iSize);
+BOOL kSetInterruptFlag(BOOL bEnableInterrupt);
 
 #endif /*__UTILITY_H__*/
